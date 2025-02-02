ab287815cf3033bf4f865d5ef1e4d8e6db91aca8
o-oayda <oliver.oayda@sydney.edu.au>
2025-02-02 18:00:43 +1100

    markdown tables in yml powers now convert to latex at script generation

--- Scripts/pwrs_to_tex.py
+++ Scripts/pwrs_to_tex.py
@@ -1,5 +1,9 @@
 import os
-from funcs import name_to_ref, integer_to_ordinal, load_yml
+from funcs import (
+    name_to_ref, integer_to_ordinal, load_yml, get_internal_latex
+)
+import re
+import mdpd
 
 yml_powers = load_yml('powers_final.yml')
 
@@ -19,6 +23,27 @@ for key, val in yml_powers.items():
     ref = name_to_ref(key, power=True)
     title = '{}{}'.format(key, ref)
 
+    # check for tables in description (search for all chars between two pipes)
+    long_description = val['Long Description'][:-1]
+    regexp = re.compile(r'(\|[\S\s]*\|)')
+    match = regexp.search(long_description)
+    if match:
+        df = mdpd.from_md(match[0])
+        latex_table = df.style.hide(axis='index').to_latex()[:-1]
+        internal_latex = get_internal_latex(latex_table)
+        internal_latex = internal_latex.replace('\\\\', '\\\\\\')
+        dnd_table_string = fr'''\\begin{{table}}[htbp]%
+    \\begin{{DndTable}}[
+        width=\\columnwidth,
+        header={val['Table Header']}
+    ]{{{val['Table Align']}}}
+        {internal_latex}
+    \\end{{DndTable}}
+\\end{{table}}'''
+        long_description = regexp.sub(
+            repl=dnd_table_string, string=long_description
+        )
+
     long_power_string = f'''\DndPowerHeader%
     {{{title}}}
     {{{hdr}}}
@@ -27,7 +52,7 @@ for key, val in yml_powers.items():
     {{{cost}}}
     {{{val['Duration']}}}
     {{{val['Requirements']}}}
-{val['Long Description'][:-1]}
+{long_description}
 {augment}'''
     strings.append(long_power_string)
 
